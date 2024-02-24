05d4263f6b845393849965d8a935b0043fa1c9c2
Vanshil Vaghasiya <vanshilpatel12@gmail.com>
2024-02-24 11:10:53 +0530

    Update Tracks.js

--- components/Tracks.js
+++ components/Tracks.js
@@ -11,7 +11,7 @@ import {
 export default function Tracks() {
   return (
     <Box marginBlock={16} id="tracks">
-      <Heading color={"#4299E1"} marginBlock={"8"}>Tracks - Previous season</Heading>
+      <Heading color={"#4299E1"} marginBlock={"8"}>Tracks</Heading>
       <Grid
         gridAutoRows="1fr"
         templateColumns={{ sm: "repeat(1, 1fr)", md: "repeat(3, 1fr)" }}
@@ -21,49 +21,25 @@ export default function Tracks() {
         <GridItem p="4">
           <TrackCard
             imageUrl="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/fd25dfdc-c699-4e4b-946c-459405520d52"
-            trackName="Paperpal - Future of Academic Writing"
+            trackName="PeopleMetrics: Shaping the next gen workforce by Revelio Labs"
           />
         </GridItem>
         <GridItem p="4">
           <TrackCard
             imageUrl="https://image.lexica.art/full_jpg/58e2902c-29dd-49ce-9149-3c4975b9d468"
-            trackName="Currency Exchange Rate Prediction - Data Science"
+            trackName="Computer Vision by JK Lakshmi Cement"
           />
         </GridItem>
         <GridItem p="4">
           <TrackCard
             imageUrl="https://image.lexica.art/full_jpg/be1f98f4-31b0-4d8b-b13b-2ed3d4d8f001"
-            trackName=" BrainWave - Natural Language Processing"
+            trackName="Cactus AI4Researchers by Cactus"
           />
         </GridItem>
         <GridItem p="4">
           <TrackCard
             imageUrl="https://image.lexica.art/full_jpg/c45219aa-514a-4a2a-9cb8-0c79f59a584b"
-            trackName="Human Face Recognition - Computer Vision"
-          />
-        </GridItem>
-        <GridItem p="4">
-          <TrackCard
-            imageUrl="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/a2c3ab34-800c-492b-aee7-836992478ec8"
-            trackName="Manufacturing"
-          />
-        </GridItem>
-        <GridItem p="4">
-          <TrackCard
-            imageUrl="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/d8d45c0e-226c-4d44-a91a-614e92b88240"
-            trackName="Stock Market Analysis"
-          />
-        </GridItem>
-        <GridItem p="4">
-          <TrackCard
-            imageUrl="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/4bf57a24-6927-44c6-a63c-b01e524a055e"
-            trackName="Querying diamond inventory through chatbots - Natural Language Processing"
-          />
-        </GridItem>
-        <GridItem p="4">
-          <TrackCard
-            imageUrl="https://image.lexica.art/full_jpg/6bf112cf-e487-46b5-aba9-b3f2249e1be9"
-            trackName="Environmental, Social, and Governance"
+            trackName="CodeGenXpert by Intech"
           />
         </GridItem>
         <GridItem p="4">
