180e2ba9e76e327285d00aacd0594e50736a83a3
Vanshil Vaghasiya <vanshilpatel12@gmail.com>
2024-02-29 09:51:37 +0530

    timerchange

--- components/Timer.js
+++ components/Timer.js
@@ -6,9 +6,30 @@ import { useCountdown } from "./Timerhook";
 
 const ExpiredNotice = () => {
   return (
-    <Box zIndex={100} fontWeight="black" bgColor="blue.200" bgGradient="linear(90deg, hsla(210, 90%, 80%, 1) 0%, hsla(212, 93%, 49%, 1) 100%)" className="expired-notice">
-      <span color="black" >  The MINeD'24 has officially begun. Let the coding frenzy commence! </span>
-    </Box>
+    // <Box zIndex={100} fontWeight="black" bgColor="blue.200" bgGradient="linear(90deg, hsla(210, 90%, 80%, 1) 0%, hsla(212, 93%, 49%, 1) 100%)" className="expired-notice">
+    //   <span color="black" >  The MINeD'24 has officially begun. Let the coding frenzy commence! </span>
+    // </Box>
+//     <Box
+//        zIndex={100}
+//        fontWeight="black"
+//        bgColor="blue.200"
+//        bgGradient="linear(90deg, hsla(210, 90%, 80%, 1) 0%, hsla(212, 93%, 49%, 1) 100%)"
+//        className="expired-notice"
+//        color="black" // or textColor="black"
+// >
+//   <span>The MINeD'24 has officially begun. Let the coding frenzy commence!</span>
+// </Box>
+    <Box
+  zIndex={100}
+  fontWeight="black"
+  bgColor="blue.200"
+  bgGradient="linear(90deg, hsla(210, 90%, 80%, 1) 0%, hsla(212, 93%, 49%, 1) 100%)"
+  className="expired-notice"
+  color="black"
+>
+  The MINeD'24 has officially begun. Let the coding frenzy commence!
+</Box>
+
   );
 };
 
