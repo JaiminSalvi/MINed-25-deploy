1e6095ab4c5262a35467d3114b165ad25db26393
Priyang1310 <139229134+Priyang1310@users.noreply.github.com>
2025-01-25 11:36:52 +0530

    Update Tracks.js

--- components/Tracks.js
+++ components/Tracks.js
@@ -11,7 +11,7 @@ import {
 export default function Tracks() {
   return (
     <Box marginBlock={16} id="tracks">
-      <Heading color={"#4299E1"} marginBlock={"8"}>Previous Tracks</Heading>
+      <Heading color={"#4299E1"} marginBlock={"8"}>Tracks</Heading>
       <Grid
         gridAutoRows="1fr"
         templateColumns={{ sm: "repeat(1, 1fr)", md: "repeat(3, 1fr)" }}
@@ -20,33 +20,33 @@ export default function Tracks() {
       >
         <GridItem p="4">
           <TrackCard
-            imageUrl="https://lexica-serve-encoded-images2.sharif.workers.dev/full_jpg/fd25dfdc-c699-4e4b-946c-459405520d52"
-            trackName="PeopleMetrics: Shaping the next gen workforce by Revelio Labs"
+            imageUrl="https://image.lexica.art/full_webp/1918c967-5c8d-4d1e-9dca-4a4eee0e03bc"
+            trackName="Playpulse AI-Powered Hightlight Extraction and Player Tracking"
           />
         </GridItem>
         <GridItem p="4">
           <TrackCard
-            imageUrl="https://image.lexica.art/full_jpg/58e2902c-29dd-49ce-9149-3c4975b9d468"
-            trackName="Computer Vision by Cactus"
+            imageUrl="https://image.lexica.art/full_webp/280c2973-c142-45f5-8c57-193bbbd4d229"
+            trackName="Academic Research Remix"
           />
         </GridItem>
         
         <GridItem p="4">
           <TrackCard
-            imageUrl="https://image.lexica.art/full_jpg/be1f98f4-31b0-4d8b-b13b-2ed3d4d8f001"
-            trackName="AI4Researchers by Cactus"
+            imageUrl="https://image.lexica.art/full_webp/19971cd7-4fdf-4bc0-a084-09979f7c38ec"
+            trackName="Smart Route Optimizer"
           />
         </GridItem>
         <GridItem p="4">
           <TrackCard
-            imageUrl="https://image.lexica.art/full_jpg/c45219aa-514a-4a2a-9cb8-0c79f59a584b"
-            trackName="CodeGenXpert by Intech"
+            imageUrl="https://image.lexica.art/full_webp/27b01d46-fcb2-4e35-806d-0cfccea1319f"
+            trackName="Manufacturing Use Case Solution With AI "
           />
         </GridItem>
         <GridItem p="4">
           <TrackCard
-            imageUrl="https://images.unsplash.com/photo-1676676701269-65de47175adf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-            trackName="Open Innovations"
+            imageUrl="https://image.lexica.art/full_webp/4d303f3d-8c9b-4066-bcea-03a63824e75a"
+            trackName="Efficient Data Extraction From Financial Reports"
           />
         </GridItem>
       </Grid>
@@ -62,7 +62,7 @@ function TrackCard({ imageUrl, trackName }) {
       borderRadius="md"
       overflow="hidden"
     >
-      <Image src={imageUrl}></Image>
+      <Image height={"400px"} width={""} src={imageUrl}></Image>
       <CardBody
         width="full"
         position="absolute"
