46920162b8e010fbbd9c4d328ad62f284be9b96d
Priyang1310 <139229134+Priyang1310@users.noreply.github.com>
2025-01-26 15:55:34 +0530

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -34,7 +34,7 @@ export default function CallToActionWithVideo() {
       >
         <Stack flex={1} spacing={{ base: 5, md: 10 }}>
           <Heading lineHeight={1.1} fontWeight={600}>
-            <Text as={"span"} fontSize={["lg", "xl"]} color={""} fontWeight="black">
+            <Text as={"span"} fontSize={["lg", "xl"]} color={"blue.400"} fontWeight="black">
               5th edition of
             </Text>
             <Text
