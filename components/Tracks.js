84f919abeba23f990fa5c112f716fad84b968e72
Priyang1310 <139229134+Priyang1310@users.noreply.github.com>
2025-01-27 10:58:12 +0530

    Update Tracks.js

--- components/Tracks.js
+++ components/Tracks.js
@@ -1,4 +1,4 @@
-import {
+8import {
   Box,
   Card,
   CardBody,
@@ -21,7 +21,7 @@ export default function Tracks() {
         <GridItem p="4">
           <TrackCard
             imageUrl="https://image.lexica.art/full_webp/1918c967-5c8d-4d1e-9dca-4a4eee0e03bc"
-            trackName="Playpulse AI-Powered Hightlight Extraction and Player Tracking"
+            trackName="Playpulse AI-Powered Highlight Extraction and Player Tracking"
           />
         </GridItem>
         <GridItem p="4">
